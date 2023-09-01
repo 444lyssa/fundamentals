12d609f79a01345b54a055c53d6e2d55a472dadb
444lyssa <143042005+444lyssa@users.noreply.github.com>
2023-09-01 10:05:01 -0600

    Add files via upload

---
+++ conditionals-and-functions.js
@@ -0,0 +1,24 @@
+/*
+ * Conditionals, Functions, Scope and Loops.
+ */
+
+// Equals
+let equals = 1 == 1;
+console.log(equals);
+
+// Greater than =
+let greaterThan = 5 > 1;
+
+// Less than
+let lessthan = 2 < 10;
+
+// Greater than Equals
+let greaterThanEq = 5 >= 5;
+
+// Less Than Equals
+let greaterthaneq = 4 <= 9;
+
+// Not Equals
+let notEquals = 5 !== 2;
+
+
---
+++ objects-apis.js
@@ -0,0 +1,16 @@
+/*
+ * Objects, Interfaces, and API'S
+ */
+
+let dog = {
+    name: "Noah",
+    color: "golden",
+    breed: "labrador",
+    size: "large",
+    bark: function(typeOfBark){
+        console.log("Bark!");
+    },
+};
+
+dog.bark();
+
